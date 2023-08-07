6631db23f5722940bb9099e53f41c88cb6ec3cca
Svetoslav <venom008@gmail.com>
2023-08-07 18:59:29 +0300

    Initial Commit

---
+++ IOExplorer/IOExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOExplorerApp.swift
+//  IOExplorer
+//
+//  Created by Svetoslav on 07.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct IOExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
